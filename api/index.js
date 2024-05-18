8cd673bb381ce9cd9d0835c4703fab73b4996907
joaovitorteixeiradesouza <49875967+joaovitorteixeiradesouza@users.noreply.github.com>
2024-05-18 15:39:57 -0300

    Correção api 4

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+
+// Caminho para o arquivo db.json original
+const dbPath = path.join(__dirname, '..', 'db.json');
+
+// Caminho para onde o db.json será copiado
+const tempDbPath = '/tmp/db.json';
+
+// Copiar db.json para o diretório /tmp na inicialização
+fs.copyFileSync(dbPath, tempDbPath);
+
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router(tempDbPath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use('/api', router);
 
-module.exports = server;
+server.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+module.exports = (req, res) => {
+  server(req, res);
+};
